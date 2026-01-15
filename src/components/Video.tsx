72057e1679b1c046be5e3178671e4a49fcdb05b8
Nuruddin <104330069+Xivlon@users.noreply.github.com>
2026-01-15 23:09:01 +0300

    Update video source URL in Video component

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -10,7 +10,7 @@ const Video: React.FC = () => {
             preload="none" 
             aria-label="A brief Introduction to the R2M Protocol"
           >
-            <source src="https://pub-4140993c3b6c43a3aaf2d64bbc6d4309.r2.dev/are_you_toxic_%20(1080p).mp4" type="video/mp4" />
+            <source src="https://videos.r2mwillfixyourhealth.com/are_you_toxic_(1080p).mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
